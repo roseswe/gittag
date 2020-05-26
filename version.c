9497fc66dd402e2612c2ff4fce31800342e7048f
roseswe <3810660+roseswe@users.noreply.github.com>
2020-05-26 18:43:23 +0200

    fix: misc changes

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
 /* : prepare-c.sh,v 1.2 2020/05/26 16:30:05 ralph Exp $ */
 #include "version.h"
-const char* git_tag="1.0.0";
-const char* git_commit="1b3a9bdba6fe610fb5b477139c1e1325d3f8e5f0";
+const char* git_tag="1.0.0-1-gc619544";
+const char* git_commit="c6195446a233662a98614d67d42e224a2e9e6c12";
