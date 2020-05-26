8e24de1d2302863c799f08d9384d19c5c84b5ae6
roseswe <3810660+roseswe@users.noreply.github.com>
2020-05-26 19:05:27 +0200

    chg: Misc

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
 /* : prepare-c.sh,v 1.2 2020/05/26 16:30:05 ralph Exp $ */
 #include "version.h"
-const char* git_tag="1.0.0-1-gc619544";
-const char* git_commit="c6195446a233662a98614d67d42e224a2e9e6c12";
+const char* git_tag="1.0.0-2-g9497fc6";
+const char* git_commit="9497fc66dd402e2612c2ff4fce31800342e7048f";
