545865605bbfb575548a58933846ccddac111f7d
roseswe <3810660+roseswe@users.noreply.github.com>
2022-02-16 20:53:05 +0100

    unknown changes made in 2021

--- main.c
+++ main.c
@@ -5,9 +5,10 @@
 int main(void)
 {
 
-    printf("Git/CVS Id Tag Demo, $Header: /home/cvs/src/c/gittag/main.c,v 1.3 2020/05/26 16:50:03 ralph Exp $ (%s)\n\n", __DATE__);
+    printf("Git/CVS Id Tag Demo, $Header: /home/cvs/src/c/gittag/main.c,v 1.4 2020/05/29 13:48:17 ralph Exp $ (%s)\n\n", __DATE__);
     printf("git_tag=%s\n", git_tag);
     printf("git_commit=%s\n", git_commit);
 
     return 0;
 }
+
--- makechangelog.sh
+++ makechangelog.sh
@@ -1,4 +1,4 @@
-## $Header:$
+## $Header: /home/cvs/src/c/gittag/makechangelog.sh,v 1.2 2020/05/29 13:48:17 ralph Exp $
 ## Baut einen ChangeLog aus den Git-Commits (logs)
 
 gitchangelog.py | tr "#" "*" | iconv -c -f CP437 | uniq > CHANGELOG.md
