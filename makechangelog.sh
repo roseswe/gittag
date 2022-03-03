e616e551200b090fbd8b93326e15c7dea49fd319
roseswe <3810660+roseswe@users.noreply.github.com>
2022-03-03 15:28:13 +0100

    fix: gitchangelog was renamed

--- makechangelog.sh
+++ makechangelog.sh
@@ -1,5 +1,5 @@
 ## $Header: /home/cvs/src/c/gittag/makechangelog.sh,v 1.2 2020/05/29 13:48:17 ralph Exp $
 ## Baut einen ChangeLog aus den Git-Commits (logs)
 
-gitchangelog.py | tr "#" "*" | iconv -c -f CP437 | uniq > CHANGELOG.md
+gitchangelog | tr "#" "*" | iconv -c -f CP437 | uniq > CHANGELOG.md
 git commit CHANGELOG.md -s -m "chg: Testcode: Automated creation of the file CHANGELOG.md !minor"
--- version.c
+++ version.c
@@ -1,4 +1,4 @@
 /* : prepare-c.sh,v 1.2 2020/05/26 16:30:05 ralph Exp $ */
 #include "version.h"
-const char* git_tag="1.0.0-2-g9497fc6";
-const char* git_commit="9497fc66dd402e2612c2ff4fce31800342e7048f";
+const char* git_tag="1.0.0-5-g5458656";
+const char* git_commit="545865605bbfb575548a58933846ccddac111f7d";
